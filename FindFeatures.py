13b94b4071b34ae5a488dbc951917d746136a4ce
soonerfan237 <soonerfan237@gmail.com>
2019-06-24 22:02:33 -0500

    Created function to read in and normalize the data across all datasets.

--- FindFeatures.py
+++ FindFeatures.py
@@ -1,6 +1,8 @@
 import glob
 import csv
 import numpy as np
+from statistics import mean
+from statistics import median
 
 training_directory = "/Users/soonerfan237/Desktop/MerckActivity/TrainingSet/"
 
@@ -11,20 +13,23 @@ feature_dict = {}
 #then i can analyze the features and determine which have low or high variability
 #the features with highest variability will be best used in the model
 
-files = glob.glob(training_directory+"ACT7*.csv")
+files = glob.glob(training_directory+"ACT*.csv")
 for file in files:
     print(file)
     with open(file, newline='') as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
         header = next(reader)[2:] #skipping header line
         print(header)
-        header_values = []
+        header_novel = [] #these are the features that have not been seen yet
+        header_novel_values = []
         for i in range(len(header)):
-            header_values.append([0])
-        header_zip = zip(header, header_values)
+            if header[i] not in feature_dict:
+                header_novel.append(header[i])
+                header_novel_values.append([0])
+        header_novel_zip = zip(header_novel, header_novel_values)
         #print(header_zip)
         #print(dict(header_zip))
-        feature_dict.update(dict(header_zip))
+        feature_dict.update(dict(header_novel_zip))
         #for feature in feature_dict:
         #    feature = []
         for row in reader:
@@ -35,5 +40,23 @@ for file in files:
             #feature_dict[]
             #data_set.append([row[2:],label])
 
-    for feature, values in feature_dict.items():
-        print(feature, np.std(np.array(values, dtype=np.float64), dtype=np.float64))
+stdev_list = []
+for feature, values in feature_dict.items():
+    stdev_list.append(np.std(np.array(values, dtype=np.float64), dtype=np.float64))
+    print(feature, np.std(np.array(values, dtype=np.float64), dtype=np.float64))
+
+print("LEN STDEV")
+print(len(stdev_list))
+zero_count = 0
+for item in stdev_list:
+    if item == 0:
+        zero_count = zero_count+1
+print("ZERO COUNT")
+print(str(zero_count))
+print("MAX STDEV")
+print(max(stdev_list))
+print("AVG STDEV")
+print(mean(stdev_list))
+print("MEDIAN STDEV")
+print(median(stdev_list))
+print("")
---
+++ NormalizeData.py
@@ -0,0 +1,47 @@
+import glob
+import csv
+import numpy as np
+from statistics import mean
+from statistics import median
+
+training_directory = "/Users/soonerfan237/Desktop/MerckActivity/TrainingSet/"
+files = glob.glob(training_directory+"ACT*.csv")
+#this part will just read the header row of each file and store a dictionary of all unique feature names and give them all a unique index
+#ill use that index so that i can store each molecule's features in a consistent order later
+feature_dict = {}
+feature_index = 0
+for file in files:
+    print(file)
+    with open(file, newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        header = next(reader)[2:] #skipping header line
+        print(header)
+        header_novel = [] #these are the features that have not been seen yet
+        header_novel_values = []
+        for i in range(len(header)):
+            if header[i] not in feature_dict:
+                header_novel.append(header[i])
+                header_novel_values.append(feature_index)
+                feature_index = feature_index + 1
+        header_novel_zip = zip(header_novel, header_novel_values)
+        feature_dict.update(dict(header_novel_zip))
+for feature, index in feature_dict.items():
+    print(feature, index)
+blank_feature_list = [0]*feature_index
+
+#now ill read in the data from all files and store their features in a list across all datasets
+molecule_dict = {}
+for file in files:
+    print(file)
+    with open(file, newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        header = next(reader)[2:] #skipping header line
+        #print(header)
+        for row in reader:
+            molecule_name = row[0][row[0].find("M_")+2:]
+            print(molecule_name)
+            if molecule_name not in feature_dict:
+                feature_dict.update([molecule_name, blank_feature_list])
+            for i in range(len(row)-2):
+                feature_name = header[i]
+                feature_dict[molecule_name][feature_dict[feature_name]] = row[i+2]
