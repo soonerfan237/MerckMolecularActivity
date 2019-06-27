3708348c9e58d3fec7bd7dee7524d8c42ed2e4b3
soonerfan237 <soonerfan237@gmail.com>
2019-06-27 16:12:09 -0500

    Finished NormalizeData function so that it reads in all feature and activity values to dictionary.

--- NormalizeData.py
+++ NormalizeData.py
@@ -1,13 +1,16 @@
 import glob
 import csv
+import re
+import pickle
 import numpy as np
 from statistics import mean
 from statistics import median
 
-training_directory = "/Users/soonerfan237/Desktop/MerckActivity/TrainingSet/"
+training_directory = "/Users/soonerfan237/Desktop/MerckActivity/TrainingSUBSet/"
 files = glob.glob(training_directory+"ACT*.csv")
 #this part will just read the header row of each file and store a dictionary of all unique feature names and give them all a unique index
 #ill use that index so that i can store each molecule's features in a consistent order later
+print("READING IN FEATURE NAMES")
 feature_dict = {}
 feature_index = 0
 for file in files:
@@ -15,7 +18,7 @@ for file in files:
     with open(file, newline='') as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
         header = next(reader)[2:] #skipping header line
-        print(header)
+        #print(header)
         header_novel = [] #these are the features that have not been seen yet
         header_novel_values = []
         for i in range(len(header)):
@@ -25,23 +28,33 @@ for file in files:
                 feature_index = feature_index + 1
         header_novel_zip = zip(header_novel, header_novel_values)
         feature_dict.update(dict(header_novel_zip))
-for feature, index in feature_dict.items():
-    print(feature, index)
-blank_feature_list = [0]*feature_index
 
+fileObject = open(training_directory+"feature_dict",'wb') # open the file for writing
+pickle.dump(feature_dict,fileObject)
+fileObject.close()
+
+print("READING IN PROPERTY AND ACTIVITY VALUES")
+#blank_feature_list = [0]*feature_index #by initializing to this, i am essentially imputing missing values with 0; maybe try other values here???? 0.5? mean of the feature?
 #now ill read in the data from all files and store their features in a list across all datasets
-molecule_dict = {}
+molecule_dict = {} #this has molecule name as key, then an array. the first index is an array of all features and the second is an array of all activities
 for file in files:
     print(file)
+    match = re.search(r'ACT([0-9]+)',file)
+    activity_index = int(match.group(1))
     with open(file, newline='') as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
-        header = next(reader)[2:] #skipping header line
+        header = next(reader)[2:] #reading header line
         #print(header)
         for row in reader:
             molecule_name = row[0][row[0].find("M_")+2:]
-            print(molecule_name)
-            if molecule_name not in feature_dict:
-                feature_dict.update({molecule_name : blank_feature_list})
+            molecule_activity = row[1]
+            #print(molecule_name)
+            if molecule_name not in molecule_dict:
+                #molecule_dict.update({molecule_name : [[0]*feature_index,[]]}) #by initializing to this, i am essentially imputing missing values with 0; maybe try other values here???? 0.5? mean of the feature?
+                molecule_dict.update({molecule_name : [[None]*feature_index,[None]*16]})
+            molecule_dict[molecule_name][1][activity_index] = molecule_activity
             for i in range(len(row)-2):
                 feature_name = header[i]
-                feature_dict[molecule_name][feature_dict[feature_name]] = row[i+2]
+                molecule_dict[molecule_name][0][feature_dict[feature_name]] = row[i+2]
+
+print("done!")
