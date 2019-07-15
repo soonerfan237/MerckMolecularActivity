26978f322931e7384d0686029faf076530d27cf8
soonerfan237 <soonerfan237@gmail.com>
2019-07-15 00:01:32 -0500

    Added function to remove outlier features.

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ import FindCorrelatedFeatures
 import RemoveOutliers
 import NeuralNet
 import ConvNeuralNet
+import GenerativeAdversarialNetwork
 import glob
 import re
+import time
 
 def main():
-
-    data_directory = "/Users/soonerfan237/Desktop/MerckActivity/TrainingSet8/"
+    start_time = time.time()
+    data_directory = "/Users/soonerfan237/Desktop/MerckActivity/TrainingSet4/"
     files = glob.glob(data_directory + "ACT*.csv")
     activity_list = []
     for file in files:
@@ -21,11 +23,14 @@ def main():
     feature_dict, molecule_dict = CombineData.CombineData(data_directory)
     molecule_dict = NormalizeActivity.NormalizeActivity(molecule_dict, activity_list)
     feature_dict_filter, molecule_dict_filter = FindVariableFeatures.FindVariableFeatures(data_directory, feature_dict, molecule_dict)
-    molecule_dict_filter = FindCorrelatedFeatures.FindCorrelatedFeatures(feature_dict_filter, molecule_dict_filter, activity_list)
+    #molecule_dict_filter = FindCorrelatedFeatures.FindCorrelatedFeatures(feature_dict_filter, molecule_dict_filter, activity_list)
     #molecule_dict_filter = RemoveOutliers.RemoveOutliers(molecule_dict_filter)
 
     for i in activity_list:
-        NeuralNet.NeuralNet(data_directory, i, molecule_dict_filter) #i corresponds to activity number to predict
+        #NeuralNet.NeuralNet(data_directory, i, molecule_dict_filter) #i corresponds to activity number to predict
         #ConvNeuralNet.ConvNeuralNet(data_directory, i, molecule_dict_filter)
-
+        GenerativeAdversarialNetwork.GenerativeAdversarialNetwork(data_directory, i, molecule_dict_filter)
+    elapsed_time = time.time() - start_time
+    print(elapsed_time)
+    print("DONE!")
 main()
